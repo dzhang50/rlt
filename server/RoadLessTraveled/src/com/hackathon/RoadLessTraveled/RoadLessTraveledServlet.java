9d31c45bfa91d8cb3c844cb4cba16982dbdb595c
dzhang50 <dzhang50@hotmail.com>
2014-11-08 14:33:23 -0500

    Added Google AppEngine boilerplate

---
+++ server/RoadLessTraveled/src/com/hackathon/RoadLessTraveled/RoadLessTraveledServlet.java
@@ -0,0 +1,13 @@
+package com.hackathon.RoadLessTraveled;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class RoadLessTraveledServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
